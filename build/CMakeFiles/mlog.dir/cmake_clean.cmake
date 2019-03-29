3a6f9aea4ec9ca034c5c86b509518c1ecc489ebc
Sanhu Li <lisanhu2014@hotmail.com>
2019-03-29 15:47:05 -0400

    first release of the mlog library

---
+++ build/CMakeFiles/mlog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mlog.dir/logger.c.o"
+  "libmlog.pdb"
+  "libmlog.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mlog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
